45153679bb68ddacdd737f574c1e6b5de8370699
osas706 <osas.omoregie@sprinble.com>
2026-02-19 10:04:01 +0100

    feat: add padding to the main application container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import CheckSymptoms from "./pages/check-symptoms"
 
 function App() {
   return (
-    <div className="min-h-screen  bg-slate-50">
+    <div className="min-h-screen  bg-slate-50 p-1">
       <Navbar />
 
       <Routes>
