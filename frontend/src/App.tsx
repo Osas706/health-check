d0a4965f294b4d9b7ee2c64dbe145d0609784f5f
osas706 <osas.omoregie@sprinble.com>
2026-02-19 10:00:58 +0100

    style: add light slate background to main app container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import CheckSymptoms from "./pages/check-symptoms"
 
 function App() {
   return (
-    <div className="min-h-screen  ">
+    <div className="min-h-screen  bg-slate-50">
       <Navbar />
 
       <Routes>
